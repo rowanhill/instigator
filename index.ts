56cf0e965d74d1a679511aa427597f7bc0b685c6
Rowan Hill <Rowan.Hill@softwire.com>
2019-04-15 14:32:03 +0100

    Better types for transformer function

--- index.ts
+++ index.ts
@@ -51,11 +51,31 @@ function consumer(inputs: ReactiveFn<any>[], execute: (...args: any[]) => void):
     return fn;
 }
 
+function transformer<T1, R>(
+    inputs: [ReactiveFn<T1>],
+    execute: (...args: [T1]) => R
+): ReactiveFn<R>;
+function transformer<T1, T2, R>(
+    inputs: [ReactiveFn<T1>, ReactiveFn<T2>],
+    execute: (...args: [T1, T2]) => R
+): ReactiveFn<R>;
+function transformer<T1, T2, T3, R>(
+    inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>],
+    execute: (...args: [T1, T2, T3]) => R
+): ReactiveFn<R>;
+function transformer<T1, T2, T3, T4, R>(
+    inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>, ReactiveFn<T4>],
+    execute: (...args: [T1, T2, T3, T4]) => R
+): ReactiveFn<R>;
+function transformer<T1, T2, T3, T4, T5, R>(
+    inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>, ReactiveFn<T4>, ReactiveFn<T5>],
+    execute: (...args: [T1, T2, T3, T4, T5]) => R
+): ReactiveFn<R>;
 function transformer<R>(inputs: ReactiveFn<any>[], execute: (...args: any[]) => R): ReactiveFn<R> {
     let lastArgs = inputs.map(_ => undefined);
     let latest: R = undefined as unknown as R;
     
-    const fn = () => {
+    const fn: ReactiveFn<R> = () => {
         const args = inputs.map(d => d());
         for (let i = 0; i < args.length; i++) {
             if (lastArgs[i] !== args[i]) {

0ab3c9c36ffd0c53eab7a3202b683e78691a5404
Rowan Hill <Rowan.Hill@softwire.com>
2019-04-15 16:45:45 +0100

    Add some Jest tests to cover basic functionality

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-interface ReactiveFn<R> {
+export interface ReactiveFn<R> {
     (updated?: R): R;
     registerConsumer: (consumer: () => void) => void;
 }
 
-function activeSource<R>(initial: R): ReactiveFn<R> {
+export function activeSource<R>(initial: R): ReactiveFn<R> {
     let latest: R = initial;
     const consumers: Set<() => void> = new Set();
     const fn: ReactiveFn<R> = function(updated?: R) {
@@ -20,29 +20,29 @@ function activeSource<R>(initial: R): ReactiveFn<R> {
     return fn;
 }
 
-function transformer<T1, R>(
+export function transformer<T1, R>(
     inputs: [ReactiveFn<T1>],
     execute: (...args: [T1]) => R
 ): ReactiveFn<R>;
-function transformer<T1, T2, R>(
+export function transformer<T1, T2, R>(
     inputs: [ReactiveFn<T1>, ReactiveFn<T2>],
     execute: (...args: [T1, T2]) => R
 ): ReactiveFn<R>;
-function transformer<T1, T2, T3, R>(
+export function transformer<T1, T2, T3, R>(
     inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>],
     execute: (...args: [T1, T2, T3]) => R
 ): ReactiveFn<R>;
-function transformer<T1, T2, T3, T4, R>(
+export function transformer<T1, T2, T3, T4, R>(
     inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>, ReactiveFn<T4>],
     execute: (...args: [T1, T2, T3, T4]) => R
 ): ReactiveFn<R>;
-function transformer<T1, T2, T3, T4, T5, R>(
+export function transformer<T1, T2, T3, T4, T5, R>(
     inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>, ReactiveFn<T4>, ReactiveFn<T5>],
     execute: (...args: [T1, T2, T3, T4, T5]) => R
 ): ReactiveFn<R>;
-function transformer<R>(inputs: ReactiveFn<any>[], execute: (...args: any[]) => R): ReactiveFn<R> {
-    let lastArgs = inputs.map(_ => undefined);
-    let latest: R = undefined as unknown as R;
+export function transformer<R>(inputs: ReactiveFn<any>[], execute: (...args: any[]) => R): ReactiveFn<R> {
+    let lastArgs = inputs.map(i => i());
+    let latest: R = execute(...lastArgs);
     
     const fn: ReactiveFn<R> = () => {
         const args = inputs.map(d => d());
@@ -64,31 +64,43 @@ function transformer<R>(inputs: ReactiveFn<any>[], execute: (...args: any[]) => 
     return fn;
 }
 
-function consumer<T1>(
+export function consumer<T1>(
     inputs: [ReactiveFn<T1>],
     execute: (...args: [T1]) => void
 ): () => void;
-function consumer<T1, T2>(
+export function consumer<T1, T2>(
     inputs: [ReactiveFn<T1>, ReactiveFn<T2>],
     execute: (...args: [T1, T2]) => void
 ): () => void;
-function consumer<T1, T2, T3>(
+export function consumer<T1, T2, T3>(
     inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>],
     execute: (...args: [T1, T2, T3]) => void
 ): () => void;
-function consumer<T1, T2, T3, T4>(
+export function consumer<T1, T2, T3, T4>(
     inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>, ReactiveFn<T4>],
     execute: (...args: [T1, T2, T3, T4]) => void
 ): () => void;
-function consumer<T1, T2, T3, T4, T5>(
+export function consumer<T1, T2, T3, T4, T5>(
     inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>, ReactiveFn<T4>, ReactiveFn<T5>],
     execute: (...args: [T1, T2, T3, T4, T5]) => void
 ): () => void;
-function consumer(inputs: ReactiveFn<any>[], execute: (...args: any[]) => void): () => void {
-    const fn = () => {
-        const args = inputs.map(d => d());
-        execute(...args);
+export function consumer(inputs: ReactiveFn<any>[], execute: (...args: any[]) => void): () => void {
+    let lastArgs = inputs.map(i => i());
+    const fn = (force: boolean = false) => {
+        const args = inputs.map(i => i());
+        if (force) {
+            execute(...args);
+            lastArgs = args;
+        } else {
+            for (let i = 0; i < args.length; i++) {
+                if (lastArgs[i] !== args[i]) {
+                    execute(...args);
+                    lastArgs = args;
+                    break;
+                }
+            }
+        }
     };
     inputs.forEach(d => d.registerConsumer(fn));
-    return fn;
+    return () => { fn(true); };
 }
---
+++ simple.test.ts
@@ -0,0 +1,123 @@
+import { consumer, activeSource, transformer } from './index';
+
+describe('a consumer with a simple source', () => {
+    it('does not invoke automatically', () => {
+        const spy = jest.fn();
+        const source = activeSource(1);
+        consumer([source], spy)
+
+        expect(spy).not.toHaveBeenCalled();
+    });
+
+    it('can be invoked manually', () => {
+        const spy = jest.fn();
+        const source = activeSource(1);
+        const cons = consumer([source], spy)
+
+        cons();
+
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('can be invoked manually multiple times', () => {
+        const spy = jest.fn();
+        const source = activeSource(1);
+        const cons = consumer([source], spy)
+
+        cons();
+        cons();
+
+        expect(spy).toHaveBeenCalledTimes(2);
+    });
+
+    it('is invoked with the new value when it\'s source changes', () => {
+        const spy = jest.fn();
+        const source = activeSource(1);
+        consumer([source], spy)
+
+        source(2);
+
+        expect(spy).toHaveBeenCalledWith(2);
+    });
+
+    it('is not invoked when the source is set to the same value', () => {
+        const spy = jest.fn();
+        const source = activeSource(1);
+        consumer([source], spy)
+
+        source(1);
+
+        expect(spy).not.toHaveBeenCalled();
+    })
+});
+
+describe('a consumer with two simple sources', () => {
+    it('is invoked when either source changes', () => {
+        const spy = jest.fn();
+        const s1 = activeSource('one');
+        const s2 = activeSource('alpha');
+        consumer([s1, s2], spy);
+
+        s1('two');
+        expect(spy).toHaveBeenCalledWith('two', 'alpha');
+
+        s2('beta');
+        expect(spy).toHaveBeenCalledWith('two', 'beta');
+    });
+});
+
+describe('a consumer with a transformer input', () => {
+    it('is invoked when the source & transformed values change', () => {
+        const spy = jest.fn();
+        const s = activeSource(1);
+        const t = transformer([s], x => x * 2);
+        consumer([t], spy);
+
+        s(2);
+
+        expect(spy).toHaveBeenCalledWith(4);
+    });
+
+    it('is not invoked when the updated source is transformed into the same value as before', () => {
+        const spy = jest.fn();
+        const s = activeSource(1);
+        const t = transformer([s], x => x % 3 === 0);
+        consumer([t], spy);
+
+        s(2);
+
+        expect(spy).not.toHaveBeenCalled();
+    });
+});
+
+describe('a consumer with multiple inputs from the same source', () => {
+    it('is only invoked once when the source changes once', () => {
+        const spy = jest.fn();
+        const s = activeSource(1);
+        const t1 = transformer([s], x => x * 3);
+        const t2 = transformer([s], x => x * 5);
+        consumer([s, t1, t2], spy);
+
+        s(2);
+
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toHaveBeenCalledWith(2, 6, 10);
+    });
+});
+
+describe('multiple consumers derived from the same source', () => {
+    it('are all updated once when the source changes once', () => {
+        const spy1 = jest.fn();
+        const spy2 = jest.fn();
+        const s = activeSource(1);
+        consumer([s], spy1);
+        consumer([s], spy2);
+
+        s(2);
+
+        expect(spy1).toHaveBeenCalledTimes(1);
+        expect(spy1).toHaveBeenCalledWith(2);
+        expect(spy2).toHaveBeenCalledTimes(1);
+        expect(spy2).toHaveBeenCalledWith(2);
+    });
+});

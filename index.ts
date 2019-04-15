c67d9a4773d58df06fb42918ce924276b181700a
Rowan Hill <Rowan.Hill@softwire.com>
2019-04-15 16:13:24 +0100

    Remove inline tests & reorder methods

--- index.ts
+++ index.ts
@@ -20,35 +20,6 @@ function activeSource<R>(initial: R): ReactiveFn<R> {
     return fn;
 }
 
-function consumer<T1>(
-    inputs: [ReactiveFn<T1>],
-    execute: (...args: [T1]) => void
-): () => void;
-function consumer<T1, T2>(
-    inputs: [ReactiveFn<T1>, ReactiveFn<T2>],
-    execute: (...args: [T1, T2]) => void
-): () => void;
-function consumer<T1, T2, T3>(
-    inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>],
-    execute: (...args: [T1, T2, T3]) => void
-): () => void;
-function consumer<T1, T2, T3, T4>(
-    inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>, ReactiveFn<T4>],
-    execute: (...args: [T1, T2, T3, T4]) => void
-): () => void;
-function consumer<T1, T2, T3, T4, T5>(
-    inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>, ReactiveFn<T4>, ReactiveFn<T5>],
-    execute: (...args: [T1, T2, T3, T4, T5]) => void
-): () => void;
-function consumer(inputs: ReactiveFn<any>[], execute: (...args: any[]) => void): () => void {
-    const fn = () => {
-        const args = inputs.map(d => d());
-        execute(...args);
-    };
-    inputs.forEach(d => d.registerConsumer(fn));
-    return fn;
-}
-
 function transformer<T1, R>(
     inputs: [ReactiveFn<T1>],
     execute: (...args: [T1]) => R
@@ -93,44 +64,31 @@ function transformer<R>(inputs: ReactiveFn<any>[], execute: (...args: any[]) => 
     return fn;
 }
 
-const foo: ReactiveFn<number> = activeSource(1);
-const bar: ReactiveFn<string> = activeSource('Hello');
-
-const twoFoo = transformer([foo], (fooVal) => fooVal * 2);
-const threeFoo = transformer([foo], (fooVal) => fooVal * 3);
-const combinedFoos = transformer([twoFoo, threeFoo], (twoVal, threeVal) => twoVal + threeVal);
-
-const printFoo = consumer([foo], (fooVal) => console.log('printFoo: ', fooVal));
-const printTwoFoo = consumer([twoFoo], (twoFooVal) => console.log('printTwoFoo: ', twoFooVal));
-const printCombinedFoo = consumer([combinedFoos], (combinedVal) => console.log('printCombinedFoo: ', combinedVal));
-const printBar = consumer([bar], (barVal) => console.log('printBar: ', barVal));
-const printAll = consumer([foo, twoFoo, threeFoo, combinedFoos, bar], (fooVal, twoFooVal, threeFooVal, combinedFooVal, barVal) => console.log('printAll: ', fooVal, twoFooVal, threeFooVal, combinedFooVal, barVal));
-
-printAll();
-console.log('');
-
-console.log('foo(1)');
-foo(1);
-console.log('');
-
-console.log('foo(2)');
-foo(2);
-console.log('');
-
-console.log('foo(3)');
-foo(3);
-console.log('');
-
-console.log('bar(World)');
-bar('World');
-console.log('');
-
-console.log('foo(3)');
-foo(3);
-console.log('');
-
-console.log('foo(30)');
-foo(30);
-console.log('');
-
-// printFoo();
+function consumer<T1>(
+    inputs: [ReactiveFn<T1>],
+    execute: (...args: [T1]) => void
+): () => void;
+function consumer<T1, T2>(
+    inputs: [ReactiveFn<T1>, ReactiveFn<T2>],
+    execute: (...args: [T1, T2]) => void
+): () => void;
+function consumer<T1, T2, T3>(
+    inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>],
+    execute: (...args: [T1, T2, T3]) => void
+): () => void;
+function consumer<T1, T2, T3, T4>(
+    inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>, ReactiveFn<T4>],
+    execute: (...args: [T1, T2, T3, T4]) => void
+): () => void;
+function consumer<T1, T2, T3, T4, T5>(
+    inputs: [ReactiveFn<T1>, ReactiveFn<T2>, ReactiveFn<T3>, ReactiveFn<T4>, ReactiveFn<T5>],
+    execute: (...args: [T1, T2, T3, T4, T5]) => void
+): () => void;
+function consumer(inputs: ReactiveFn<any>[], execute: (...args: any[]) => void): () => void {
+    const fn = () => {
+        const args = inputs.map(d => d());
+        execute(...args);
+    };
+    inputs.forEach(d => d.registerConsumer(fn));
+    return fn;
+}
